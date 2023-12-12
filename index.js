ec0166b0fef0a45a8524126b734b4ed8a1ea7354
mentalNika <mandrygina798@mail.ru>
2023-12-12 23:28:28 +0300

    fix

--- index.js
+++ index.js
@@ -1,106 +1,3 @@
-// const cityInput = document.querySelector(".city-input");
-// const searchButton = document.querySelector(".search-btn");
-// const currentWeatherDiv = document.querySelector(".current-weather");
-// const weatherCardsDiv = document.querySelector(".weather-cards");
-
-// const API_KEY = "adf4671e71150cb5760619e2712cf111"; // Ключ open weather map
-
-// const createWeatherCard = (cityName, weatherItem, index) => {
-//   if (index === 0) {
-//     // HTML для главной погодной карточки
-//     return `<div class="details">
-//               <h2> ${cityName} ${weatherItem.dt_txt.split(" ")[0]}</h2>
-//               <p>Температура: ${weatherItem.weather[0].icon}°C</p>
-//               <p>Ветер: ${weatherItem.wind.speed} м/с</p>
-//               <p>Влажность: ${weatherItem.main.humidity}%</p>
-//             </div>
-//             <div class="icon">
-//               <img
-//               src="https://openweathermap.org/img/wn/${
-//                 weatherItem.weather[0].icon
-//               }@4x.png"
-//               alt="иконка погоды"
-//               />
-//               <p>${weatherItem.weather[0].description}</p>
-//             </div>`;
-//   } else {
-//     // HTML для 5-и погодных карточек
-//     return `<li class="card">
-//               <h3>${weatherItem.dt_txt.split(" ")[0]}</h3>
-//               <img
-//                 src="https://openweathermap.org/img/wn/${
-//                   weatherItem.weather[0].icon
-//                 }@2x.png"
-//                 alt="иконка погоды"
-//               />
-//               <p>Темп: ${(weatherItem.main.temp - 273.15).toFixed(
-//                 2
-//               )}°          C</p>
-//               <p>Ветер: ${weatherItem.wind.speed} м/с</p>
-//               <p>Влажность: ${weatherItem.main.humidity}%</p>
-//             </li>`;
-//   }
-// };
-
-// const getWeatherDetails = (cityName, lat, lon) => {
-//   const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&lang={ru}`;
-
-//   fetch(WEATHER_API_URL)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       // Фильтр, который получает только один прогноз погоды в день
-//       const uniqueForecastDays = [];
-//       const fiveDaysForecast = data.list.filter((forecast) => {
-//         const forecastDate = new Date(forecast.dt_txt).getDate();
-//         if (!uniqueForecastDays.includes(forecastDate)) {
-//           return uniqueForecastDays.push(forecastDate);
-//         }
-//       });
-
-//       // Очистить старую инфу о погоду
-//       cityInput.value = "";
-//       currentWeatherDiv.innerHTML = "";
-//       weatherCardsDiv.innerHTML = "";
-
-//       // Создание погодных карточек и добавление их в DOM
-//       fiveDaysForecast.forEach((weatherItem, index) => {
-//         if (index === 0) {
-//         } else {
-//           currentWeatherDiv.insertAdjacentHTML(
-//             "beforeend",
-//             createWeatherCard(cityName, weatherItem, index)
-//           );
-//         }
-//         weatherCardsDiv.insertAdjacentHTML(
-//           "beforeend",
-//           createWeatherCard(cityName, weatherItem, index)
-//         );
-//       });
-//     })
-//     .catch(() => {
-//       alert("Ошибка");
-//     });
-// };
-
-// const getCityCoordinates = () => {
-//   const cityName = cityInput.value.trim(); // Получает информацию, веденную пользователем в поле input
-//   if (!cityName) return; // Возвращает, если поле пустое
-//   const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
-
-//   fetch(GEOCODING_API_URL)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       if (!data.length) return alert(`Ошибка, не найден город ${cityName}`);
-//       const { name, lat, lon } = data[0];
-//       getWeatherDetails(name, lat, lon);
-//     })
-//     .catch(() => {
-//       alert("Ошибка");
-//     });
-// };
-
-// searchButton.addEventListener("click", getCityCoordinates);
-
 const cityInput = document.querySelector(".city-input");
 const searchButton = document.querySelector(".search-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
@@ -114,7 +11,7 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                     <h6>Температура: ${(weatherItem.main.temp - 273.15).toFixed(
                       2
                     )}°C</h6>
-                    <h6>Ветер: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Ветер: ${weatherItem.wind.speed} м/с</h6>
                     <h6>Влажность: ${weatherItem.main.humidity}%</h6>
                 </div>
                 <div class="icon">
@@ -133,13 +30,13 @@ const createWeatherCard = (cityName, weatherItem, index) => {
                     <h6>Темп: ${(weatherItem.main.temp - 273.15).toFixed(
                       2
                     )}°C</h6>
-                    <h6>Ветер: ${weatherItem.wind.speed} M/S</h6>
+                    <h6>Ветер: ${weatherItem.wind.speed} м/с</h6>
                     <h6>Влажность: ${weatherItem.main.humidity}%</h6>
                 </li>`;
   }
 };
 const getWeatherDetails = (cityName, latitude, longitude) => {
-  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&lang=ru&appid=${API_KEY}`;
+  const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&lang=ru`;
   fetch(WEATHER_API_URL)
     .then((response) => response.json())
     .then((data) => {
@@ -172,7 +69,7 @@ const getWeatherDetails = (cityName, latitude, longitude) => {
 const getCityCoordinates = () => {
   const cityName = cityInput.value.trim();
   if (cityName === "") return;
-  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&lang=ru&limit=1&appid=${API_KEY}`;
+  const API_URL = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}&lang=ru`;
 
   // Get entered city coordinates (latitude, longitude, and name) from the API response
   fetch(API_URL)
@@ -192,3 +89,7 @@ cityInput.addEventListener(
   "keyup",
   (e) => e.key === "Enter" && getCityCoordinates()
 );
+
+// createWeatherCard(Москва);
+// getWeatherDetails(Москва);
+// getCityCoordinates(Москва);
